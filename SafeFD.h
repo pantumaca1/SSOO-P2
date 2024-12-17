1cf9bcfc4db7449eb900f99b2858291dbf09c8f7
Pantu <158054205+pantumaca1@users.noreply.github.com>
2024-12-17 16:31:12 +0000

    Add files via upload

---
+++ SafeFD.h
@@ -0,0 +1,30 @@
+#pragma once
+
+class SafeFD{
+  public:
+    explicit SafeFD(int fd) noexcept : fd_{fd}{}
+    explicit SafeFD() noexcept : fd_{-1}{};
+    SafeFD(const SafeFD&) = delete;
+    SafeFD& operator=(const SafeFD&) = delete;
+    SafeFD(SafeFD&& other) noexcept : fd_{other.fd_}{
+      other.fd_ = -1;
+    }
+    SafeFD& operator=(SafeFD&& other) noexcept{
+      if (this != &other && fd_ != other.fd_){
+        // Cerrar el descriptor de archivo actual
+        close(fd_);
+        // Mover el descriptor de archivo de 'other' a este objeto
+        fd_ = other.fd_;
+        other.fd_ = -1;
+      }
+      return *this;
+    }
+    ~SafeFD() noexcept{
+      if (fd_ >= 0) close(fd_);
+    }
+    [[nodiscard]] int get() const noexcept{
+      return fd_;
+    }
+  private:
+    int fd_;
+};
